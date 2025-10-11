00a42f41b25d43e3e25a60a25c24de05788ca4ee
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-10-11 10:45:25 +0300

    find closest value to x

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
+#include <climits>
 #include <iostream>
 
 int main() {
+  long long X = 0;
+  std::cin >> X;
+
   int N = 0;
   std::cin >> N;
+  long long *nums = new long long[N];
 
-  for (int i = 1; i <= N; ++i) {
-    std::cout << i << std::endl;
-    if (i % 5 == 0 && i % 3 == 0) {
-      std::cout << "foo bar\n";
-    } else if (i % 3 == 0) {
-      std::cout << "foo\n";
-    } else if (i % 5 == 0) {
-      std::cout << "bar\n";
+  for (int i = 0; i < N; ++i) {
+    int num = 0;
+    std::cin >> num;
+    nums[i] = num;
+  }
+
+  long long closest_val = nums[0];
+  long long smallest_diff = INT_MAX;
+
+  for (int i = 0; i < N; ++i) {
+    long long new_diff = std::abs(X - nums[i]);
+
+    if (new_diff == smallest_diff || new_diff < smallest_diff) {
+      closest_val = nums[i];
+      smallest_diff = new_diff;
     }
   }
+
+  std::cout << closest_val << std::endl;
+
+  delete[] nums;
 }
