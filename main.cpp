f459783684d132176921925a58d9d31c1be53d13
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-01 11:43:49 +0300

    binary to decimal converter

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
+#include <cstring>
 #include <iostream>
 
 int main() {
-  const int coefs[] = {1, 2, 1};
-  int x;
-  std::cin >> x;
-  int coefs_len = sizeof(coefs) / sizeof(coefs[0]);
+  char str[65] = {};
+  std::cin >> str;
+  unsigned int str_len = strlen(str);
+  unsigned long long result = 0;
 
-  // coef(n - 1) * x ^ {n - 1} + coef(n - 2) * x ^ {n - 2} + ... coef(1)
+  int power = 1;
+  for (int i = str_len - 1; i >= 0; --i) {
+    int cur_char_as_int;
 
-  // int x_raised = 1;
-  // for (int i = 0; i < coefs_len; ++i) {
-  //   sum += coefs[coefs_len - i - 1] * x_raised;
-  //   x_raised *= x;
-  // }
-  //
-  // a3 * x3 + a2 * x2 + a1 * x1 + a0
-  // sum = a0
-  // 3x^3+4x^2+6x+6
-  // coef = {6,5,4,3}
-  // n = 4
-  // x =1
-  // 1 iter: sum = 3
-  // sum = 3 * 1 + 4 = 7
-  // sum = (3 * 1 + 4)*1 + 5
-  // sum = ((3 * 1 + 4)*1 + 5)*1 + 6
+    if (str[i] == '1') {
+      cur_char_as_int = 1;
+    } else {
+      cur_char_as_int = 0;
+    }
 
-  int sum = coefs[coefs_len - 1];
-  for (int i = coefs_len - 2; i >= 0; --i) {
-    sum = sum * x + coefs[i];
+    result += cur_char_as_int * power;
+    power *= 2;
   }
-  std::cout << sum << std::endl;
 
-  char str[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
-  std::cout << sizeof(str) << std::endl;
-  std::cout << str << std::endl;
-
+  std::cout << result << std::endl;
   return 0;
 }
