34dbd274c4fe05e531a191af7cf323c7b4905bca
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-01 19:14:26 +0300

    contest 2: problem H

--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
 
-// 0 1 2 3 4 5 7 0 1
+#define ull unsigned long long
 
 int main() {
-  unsigned long long fib_n = 0;
-  unsigned int modulo = 0;
-  std::cin >> fib_n >> modulo;
+  ull numerator, denominator;
+  std::cin >> numerator >> denominator;
 
-  unsigned int seq_len = 0;
-  unsigned int fib_a = 0;
-  unsigned int fib_b = 1;
-
+  ull remainder = 0;
   do {
-    ++seq_len;
-
-    unsigned int new_fib_b = (fib_a + fib_b) % modulo;
-    fib_a = fib_b;
-    fib_b = new_fib_b;
-  } while (fib_a != 0 || fib_b != 1);
-
-  fib_n = fib_n - seq_len * (fib_n / seq_len);
-
-  fib_a = 0;
-  fib_b = 1;
-  for (int i = 0; i < fib_n; ++i) {
-    unsigned int new_fib_b = (fib_a + fib_b) % modulo;
-    fib_a = fib_b;
-    fib_b = new_fib_b;
-  }
-
-  std::cout << fib_a << '\n';
+    std::cout << numerator / denominator << " ";
+    remainder = numerator % denominator;
+    numerator = denominator;
+    denominator = remainder;
+  } while (remainder != 0);
 
   return 0;
 }
