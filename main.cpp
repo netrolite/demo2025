dfbefc605761c852e5c619ddc92ea54ce24e5274
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-02 02:08:30 +0300

    solution

--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
-#include <cstring>
 #include <iostream>
 
-// A -> T
-// T -> A
-// C -> G
-// G -> C
-void DNAStrand(char *dna) {
-  for (int i = 0; i < strlen(dna); ++i) {
-    if (dna[i] == 'A')
-      dna[i] = 'T';
-    else if (dna[i] == 'T')
-      dna[i] = 'A';
-    else if (dna[i] == 'C')
-      dna[i] = 'G';
-    else if (dna[i] == 'G')
-      dna[i] = 'C';
-  }
-
-  std::cout << dna;
-}
+#define ull unsigned long long
 
 int main() {
-  char dna1[] = "ATTGC";
-  DNAStrand(dna1);
+  ull radius;
+  std::cin >> radius;
+
+  ull points_inside = 0;
+  ull x = radius;
+  ull rad_squared = radius * radius;
+
+  for (ull y = 0; y <= radius; ++y) {
+    while (true) {
+      if (x * x + y * y <= rad_squared) {
+        points_inside += x;
+        break;
+      } else {
+        --x;
+      }
+    }
+  }
+
+  points_inside = points_inside * 4 + 1;
+  std::cout << points_inside << std::endl;
 
   return 0;
 }
