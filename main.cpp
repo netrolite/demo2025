cb168aa87f2c8318f1df98421fbc50ae00a9c489
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-01 10:51:45 +0300

    find value of polynomial (fastest solution)

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@ int main() {
 
   // coef(n - 1) * x ^ {n - 1} + coef(n - 2) * x ^ {n - 2} + ... coef(1)
 
-  int sum = 0;
-  int x_raised = 1;
-  for (int i = 0; i < coefs_len; ++i) {
-    sum += coefs[coefs_len - i - 1] * x_raised;
-    x_raised *= x;
-  }
+  // int x_raised = 1;
+  // for (int i = 0; i < coefs_len; ++i) {
+  //   sum += coefs[coefs_len - i - 1] * x_raised;
+  //   x_raised *= x;
+  // }
+  //
+  // a3 * x3 + a2 * x2 + a1 * x1 + a0
+  // sum = a0
+  // 3x^3+4x^2+6x+6
+  // coef = {6,5,4,3}
+  // n = 4
+  // x =1
+  // 1 iter: sum = 3
+  // sum = 3 * 1 + 4 = 7
+  // sum = (3 * 1 + 4)*1 + 5
+  // sum = ((3 * 1 + 4)*1 + 5)*1 + 6
 
+  int sum = coefs[coefs_len - 1];
+  for (int i = coefs_len - 2; i >= 0; --i) {
+    sum = sum * x + coefs[i];
+  }
   std::cout << sum << std::endl;
 
   return 0;
