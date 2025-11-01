07f786670f5b949d2b3bb6736ea02ba1f5bb865a
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-01 23:32:16 +0300

    contest 3: problem A

--- main.cpp
+++ main.cpp
@@ -1,37 +1,28 @@
+#include <cstring>
 #include <iostream>
 
+// A -> T
+// T -> A
+// C -> G
+// G -> C
+void DNAStrand(char *dna) {
+  for (int i = 0; i < strlen(dna); ++i) {
+    if (dna[i] == 'A')
+      dna[i] = 'T';
+    else if (dna[i] == 'T')
+      dna[i] = 'A';
+    else if (dna[i] == 'C')
+      dna[i] = 'G';
+    else if (dna[i] == 'G')
+      dna[i] = 'C';
+  }
+
+  std::cout << dna;
+}
+
 int main() {
-  unsigned int fib_nums[47] = {1, 2};
-  unsigned short fib_bits[47] = {};
-  unsigned long n;
-  std::cin >> n;
+  char dna1[] = "ATTGC";
+  DNAStrand(dna1);
 
-  if (n <= 2) {
-    if (n == 1)
-      std::cout << "1" << std::endl;
-    else
-      std::cout << "10" << std::endl;
-    return 0;
-  }
-
-  unsigned int last_fib_num_idx = 1;
-  for (; fib_nums[last_fib_num_idx] < n; ++last_fib_num_idx) {
-    fib_nums[last_fib_num_idx + 1] =
-        fib_nums[last_fib_num_idx - 1] + fib_nums[last_fib_num_idx];
-  }
-
-  fib_bits[last_fib_num_idx - 1] = 1;
-  n -= fib_nums[last_fib_num_idx - 1];
-
-  for (int i = last_fib_num_idx - 2; n != 0; --i) {
-    if (fib_nums[i] <= n) {
-      n -= fib_nums[i];
-      fib_bits[i] = 1;
-    }
-  }
-
-  for (int i = last_fib_num_idx - 1; i >= 0; --i) {
-    std::cout << fib_bits[i];
-  }
-  std::cout << std::endl;
+  return 0;
 }
