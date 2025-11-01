a0239761badf46819eea8ccf1d001ac7073f416a
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-01 14:05:27 +0300

    testing pointers

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 
 int main() {
   int arr[] = {1, 3, 5};
-  std::cout << arr[1] << ' ' << *(arr + 1) << '\n';
-  std::cout << 1 [arr] << '\n';
+  // std::cout << arr[1] << ' ' << *(arr + 1) << '\n';
+  // std::cout << 1 [arr] << '\n';
+  int n = 69696969;
 
-  *(arr + 1) = 1000;
-
-  std::cout << arr[1] << ' ' << *(arr + 1) << '\n';
-
-  int n = 10;
-  std::cout << n << '\n';
-  int *ptr_n = &n;
-  *ptr_n = 300;
-  std::cout << n << '\n';
+  for (int i = -10; i < 10; ++i) {
+    std::cout << *(arr + i) << '\n';
+  }
+  // *(arr + 1) = 1000;
+  //
+  // std::cout << arr[1] << ' ' << *(arr + 1) << '\n';
+  //
+  // int n = 10;
+  // std::cout << n << '\n';
+  // int *ptr_n = &n;
+  // *ptr_n = 300;
+  // std::cout << n << '\n';
 
   return 0;
 }
