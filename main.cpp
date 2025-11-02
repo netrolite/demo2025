d55cb28bcdcdb473d04b5e046529bb25458b7c36
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-02 11:56:33 +0300

    contest 3: task B

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
+#include <cstring>
 #include <iostream>
 
-#define ull unsigned long long
+unsigned int strsum(const char *str) {
+  unsigned long long sum = 0;
 
-int main() {
-  ull radius;
-  std::cin >> radius;
-
-  ull points_inside = 0;
-  ull x = radius;
-  ull rad_squared = radius * radius;
-
-  for (ull y = 0; y <= radius; ++y) {
-    while (true) {
-      if (x * x + y * y <= rad_squared) {
-        points_inside += x;
-        break;
-      } else {
-        --x;
-      }
-    }
+  for (unsigned long long i = 0; i < strlen(str); ++i) {
+    sum += str[i];
   }
 
-  points_inside = points_inside * 4 + 1;
-  std::cout << points_inside << std::endl;
-
-  return 0;
+  return sum;
 }
+
+int main() { std::cout << strsum("Hello, world!") << '\n'; }
