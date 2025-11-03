aaa3491832f029efa4e42623df79b8382c072465
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-03 13:44:57 +0300

    wip

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
 #include <cstring>
 #include <iostream>
 
-bool is_isogram(const char *str) {
-  unsigned int len = strlen(str);
-  if (len == 0)
-    return true;
+// world
+// worldkq
 
-  unsigned int char_occuurences[128] = {};
+// hello
+// ehelol
+/*
+  target:
+  h - 1
+  e - 1
+  l - 2
+  o - 1
+
+  given string:
+  h - 1
+  e - 2
+  l - 2
+  o - 1
+*/
+
+char *str_to_lower(char str[]) {
+  unsigned int len = strlen(str);
+
   for (unsigned int i = 0; i < len; ++i) {
     char cur_char = str[i];
     bool is_uppercase = cur_char >= 65 and cur_char <= 90;
     if (is_uppercase) {
-      cur_char += 32; // offset to make lowercase
+      str[i] += 32; // offset to make lowercase
     }
-
-    if (char_occuurences[cur_char] == 1)
-      return false;
-    char_occuurences[cur_char] = 1;
   }
 
+  return str;
+}
+
+bool is_subset(const char *str1, const char *target) {
+  char target_char_occurences[127]{};
+  char str_char_occurences[127]{};
   return true;
 }
 
 int main() {
-  std::cout << is_isogram("iiiiiii") << '\n';
-  std::cout << is_isogram("uncopyrightable") << '\n';
-  std::cout << is_isogram("mo0se") << '\n';
-  std::cout << is_isogram("fgzbma") << '\n';
-  return 0;
+  char target[] = "hEEllo";
+  char str[] = "Elellhlo";
+  char *target_lower = str_to_lower(target);
+  char *str_lower = str_to_lower(str);
 }
