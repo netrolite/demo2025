56ac52b4fd11b00a37216bd2c9beeb9550bd17f9
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2025-11-08 13:46:34 +0300

    wip

--- main.cpp
+++ main.cpp
@@ -1,24 +1,63 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; ++i) {
-    for (int j = 0; j < strlen(argv[i]); ++j) {
-      char c = argv[i][j];
-      bool is_uppercase = c >= 'a' and c <= 'z'; // matvey moroZOV
-      if (is_uppercase) {
-        std::cout << (char)(c - 'a' + 'A');
-      } else {
-        std::cout << c;
-      }
+bool starts_with(char *src, char *target) {
+  for (int i = 0; i < strlen(target); ++i) {
+    if (target[i] != src[i])
+      return false;
+  }
+  return true;
+}
+
+int is_substr(char *a, char *b) {
+  // a = "hello";
+  // b = "ll";
+  int i = 0;
+  while (*a != 0) {
+    if (starts_with(a, b)) {
+      return i;
     }
-    std::cout << '\n';
+    ++a;
+    ++i;
   }
 
-  char c1 = 127;
-  char c2 = 200;
-  std::cout << c1 + c2 << '\n';
-  auto y = c1 + c2;
+  return false;
+}
+
+void replace_one(char *str) {
+  char one[] = "one";
+  // hiohihi
+  // hi1hi
+
+  while (*str != 0) {
+    if (starts_with(str, one)) {
+      *str = '1';
+      for (int j = 1; j < strlen(str); ++j) {
+        str[j] = str[j + 2];
+      }
+      return;
+    }
+    ++str;
+  }
+}
+
+char *replace(char *str, char *target, char *replacement, char *buf) {
+  for (int i = 0; i < strlen(str); ++i) {
+    buf[i] = str[i];
+  }
+
+  std::cout << buf << std::endl;
+  return buf;
+}
+
+int main(int argc, char *argv[]) {
+  char *str = argv[1];
+  char *target = argv[2];
+  char *replacement = argv[3];
+  char buf[128] = {};
+  char *result = replace(str, target, replacement, buf);
+
+  std::cout << result << std::endl;
 
   return 0;
 }
